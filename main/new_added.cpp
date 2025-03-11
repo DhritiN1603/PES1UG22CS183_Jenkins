6544df4047bb30007e16490ecfafddb2edef7dd2
DhritiN1603 <dhriti1604@gmail.com>
2025-03-11 17:35:38 +0530

    new_cpp

---
+++ main/new_added.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built the pipeline and run this for lab-8  \n";
+  cout << "My SRN is PES1UG22CS183\n"; 
+  cout << "This was fun\n";
+  return 0;
+}
