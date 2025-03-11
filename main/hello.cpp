30fb7a0c3249222b2caac6197f1393978a03e454
DhritiN1603 <dhriti1604@gmail.com>
2025-03-11 16:40:09 +0530

    Second update

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this for lab-8  \n";
-  cout << "My SRN is PES1UG22CS183"
+  cout << "My SRN is PES1UG22CS183\n"; 
   return 0;
 }
